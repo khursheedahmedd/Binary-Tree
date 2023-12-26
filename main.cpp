4a1b94aa6a549e6463a52dd74650bd042378f958
Pro-Programmer38 <118465990+Pro-Programmer38@users.noreply.github.com>
2023-12-26 18:28:21 +0500

    added menu.

--- main.cpp
+++ main.cpp
@@ -5,4 +5,22 @@
 //  Created by Macbook on 26/12/2023.
 //
 
+#include "BinaryTree.hpp"
 
+int main() {
+    
+    BinaryTree* tree = new BinaryTree();
+    
+    int choice;
+    char ch = 'y';
+    
+    do {
+        cout<<"\n******* Binary Tree ********";
+        cout<<"\n* 1. Insert a node         *";
+        cout<<"\n* 2. Display Tree          *";
+        cout<<"\n* 3. Exit                  *";
+        cout<<"\n****************************";
+        cout<<"\nEnter your choiec: ";
+        cin>>choice;
+    }
+}
