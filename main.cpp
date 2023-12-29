6e816213754649e6be55bce9d19343371d64f83d
Pro-Programmer38 <118465990+Pro-Programmer38@users.noreply.github.com>
2023-12-29 18:47:24 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,65 +7,21 @@
 
 #include "BinaryTree.hpp"
 
+
 int main() {
-    
     BinaryTree* tree = new BinaryTree();
+
+
+    int values[] = {50, 30, 70, 20, 40, 60, 80};
     
-    int choice;
-    char ch = 'y';
-    
-    do {
-        cout<<"\n******* Binary Tree ********";
-        cout<<"\n* 1. Insert a node         *";
-        cout<<"\n* 2. Display Tree          *";
-        cout<<"\n* 3. Search                *";
-        cout<<"\n* 4. Find Minimum          *";
-        cout<<"\n* 5. Find Maximum          *";
-        cout<<"\n* 6. Exit                  *";
-        cout<<"\n****************************";
-        cout<<"\nEnter your choiec: ";
-        cin>>choice;
-        
-        switch(choice) {
-            case 1: {
-                int num;
-                cout<<"\nEntre a number to insert: ";
-                cin>>num;
-                
-                tree->insert(num);
-                break;
-            }
-            case 2: {
-                tree->display();
-                break;
-            }
-            case 3: {
-                int num;
-                cout<<"\nEnter a number to search: ";
-                cin>>num;
-                tree->search(num);
-                break;
-            }
-            case 4: {
-                cout<<"\nMinimum number is: "<<tree->findMin();
-                break;
-            }
-            case 5: {
-                cout<<"\nThe Maximum number is: "<<tree->findMax();
-                break;
-            }
-            case 6: {
-                cout<<"\nExiting....\n";
-                return 0;
-            }
-            default:
-                cout<<"\nInvalid choice.";
-        }
-        cout<<"\nDo you want to repeat? Press Y: ";
-        cin>>ch;
-    }while(ch == 'Y' || ch == 'y');
-    
+    for (int value : values) {
+        tree->insert(value);
+    }
+
+    cout << "\nDisplaying the tree with hardcoded values:" << endl;
+    tree->display();
+
     delete tree;
-    
+
     return 0;
 }
