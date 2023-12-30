b70a2a980f18d974edab01f388ced41549ea81c7
Pro-Programmer38 <118465990+Pro-Programmer38@users.noreply.github.com>
2023-12-30 22:20:45 +0500

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -7,58 +7,71 @@
 
 #include "BinaryTree.hpp"
 
-
-BinaryTree::BinaryTree() {
+BinaryTree::BinaryTree()
+{
     root = NULL;
     count = 0;
 }
 
-BinaryTree::~BinaryTree() {
+BinaryTree::~BinaryTree()
+{
     delete root;
 }
 
-void BinaryTree::insert(int num) {
-    TreeNode* newNode = new TreeNode();
+void BinaryTree::insert(int num)
+{
+    TreeNode *newNode = new TreeNode();
     newNode->setData(num);
-    
-    if (root == nullptr) {
+
+    if (root == nullptr)
+    {
         root = newNode;
         std::cout << "\nThe new node is inserted successfully.";
         return;
     }
 
-//    TreeNode* temp = root;
-    TreeNode* current = root;
-    TreeNode* parent = nullptr;
+    //    TreeNode* temp = root;
+    TreeNode *current = root;
+    TreeNode *parent = nullptr;
 
-    while (current != nullptr) {
+    while (current != nullptr)
+    {
         parent = current;
-        if (num < current->getData()) {
+        if (num < current->getData())
+        {
             current = current->getLeft();
-        } else if (num > current->getData()) {
+        }
+        else if (num > current->getData())
+        {
             current = current->getRight();
-        } else {
+        }
+        else
+        {
             std::cout << "\nNode with the same value already exists.";
             return;
         }
     }
 
-    if (num < parent->getData()) {
+    if (num < parent->getData())
+    {
         parent->setLeft(newNode);
-    } else {
+    }
+    else
+    {
         parent->setRight(newNode);
     }
-    
-    std::cout << "\nThe new node is inserted successfully.";
+
+    // std::cout << "\nThe new node is inserted successfully.";
 }
 
-void BinaryTree::display() {
-    if(root == NULL) {
-        cout<<"\nThe tree is empty.";
+void BinaryTree::display()
+{
+    if (root == NULL)
+    {
+        cout << "\nThe tree is empty.";
         return;
     }
-    
-   
+
     TreeNode *current = root;
     TreeNode *pre;
 
@@ -93,49 +106,64 @@ void BinaryTree::display() {
     cout << endl;
 }
 
-void BinaryTree::search(int num) {
-    if(root == nullptr) {
+void BinaryTree::search(int num)
+{
+    if (root == nullptr)
+    {
         cout << "\nThe tree is empty.";
         return;
     }
-    
-    TreeNode* temp = root;
-    while(temp != nullptr) {
-        if(num == temp->getData()) {
+
+    TreeNode *temp = root;
+    while (temp != nullptr)
+    {
+        if (num == temp->getData())
+        {
             cout << "\nFound " << num << " in the tree.";
             return;
-        } else if(num < temp->getData()) {
+        }
+        else if (num < temp->getData())
+        {
             temp = temp->getLeft();
-        } else {
+        }
+        else
+        {
             temp = temp->getRight();
         }
     }
 
-    cout << "\n" << num << " not found in the tree.";
+    cout << "\n"
+         << num << " not found in the tree.";
 }
 
-int BinaryTree::findMin() {
-    if(root == nullptr) {
+int BinaryTree::findMin()
+{
+    if (root == nullptr)
+    {
         std::cout << "\nThe tree is empty.";
         return INT_MIN;
     }
 
-    TreeNode* current = root;
-    while(current->getLeft() != nullptr) {
+    TreeNode *current = root;
+    while (current->getLeft() != nullptr)
+    {
         current = current->getLeft();
     }
 
     return current->getData();
 }
 
-int BinaryTree::findMax() {
-    if(root == nullptr) {
+int BinaryTree::findMax()
+{
+    if (root == nullptr)
+    {
         std::cout << "\nThe tree is empty.";
-        return INT_MAX; 
+        return INT_MAX;
     }
 
-    TreeNode* current = root;
-    while(current->getRight() != nullptr) {
+    TreeNode *current = root;
+    while (current->getRight() != nullptr)
+    {
         current = current->getRight();
     }
 
